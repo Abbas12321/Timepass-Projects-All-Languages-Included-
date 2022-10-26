8de793e32120f5bb129286a86329615c3ec6a96f
Abbas Shaikh <61411096+Abbas12321@users.noreply.github.com>
2022-10-26 22:45:01 +0530

    Add files via upload

---
+++ TodoList/src/ToDoList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ToDoList = () => {
+    return(
+    <>
+        <div className="main_div">
+        <div className="center_div">
+        <h1> 0 </h1>
+        <div className="btn_div">
+            <button> Incre </button>
+            <button> Decre </button>
+        </div>
+        </div>
+        </div>
+    </>
+    )
+}
+
+export default ToDoList;
